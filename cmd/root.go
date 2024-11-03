e16cf91827aba2b2f0790d1d3650c4f77651b95c
Patrick F. Marques <patrickfmarques@gmail.com>
2024-11-03 16:49:20 +0000

    Improve cli help with installed plugins list

--- cmd/root.go
+++ cmd/root.go
@@ -13,6 +13,46 @@ var (
 	Version   = "dev"
 )
 
+// Similar to UsageTemplate() from cobra command, but with additional "Available Plugins" list.
+const usageTemplate = `Usage:{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}{{$cmds := .Commands}}{{if eq (len .Groups) 0}}
+
+Available Commands:{{range $cmds}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{else}}{{range $group := .Groups}}
+
+{{.Title}}{{range $cmds}}{{if (and (eq .GroupID $group.ID) (or .IsAvailableCommand (eq .Name "help")))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if not .AllChildCommandsHaveGroup}}
+
+Additional Commands:{{range $cmds}}{{if (and (eq .GroupID "") (or .IsAvailableCommand (eq .Name "help")))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+{{- if (and (not .HasParent) (gt (len pluginList) 0))}}
+
+Available Plugins:
+{{- range pluginList}}
+  {{.}}
+{{- end}}
+{{- end}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`
+
 const versionTemplate = `
 {{- with .Name}}{{printf "%s " .}}{{end}}{{printf "version: %s" .Version -}}
 
@@ -33,6 +73,8 @@ func Execute() {
 		Short:   "Calculator is a simple CLI calculator as a cobra example",
 		Version: Version,
 	}
+
+	rootCmd.SetUsageTemplate(usageTemplate)
 
 	cobra.AddTemplateFunc("GitCommit", func() string { return GitCommit })
 	rootCmd.SetVersionTemplate(versionTemplate)
